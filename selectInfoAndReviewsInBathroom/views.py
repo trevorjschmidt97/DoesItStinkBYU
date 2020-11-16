b35c99e10a3a11fbe449d1f7485d7e47551126bf
Trevor Schmidt <trevorjschmidt97@gmail.com>
2020-11-15 22:08:31 -0700

    Finished server

--- selectInfoAndReviewsInBathroom/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from DoesItStinkBYUData import selectInfoAndReviewsInBathroom
-import json
-
-def indexPageView(request, bathroomID):
-    infoAndReviews = selectInfoAndReviewsInBathroom(bathroomID)
-    return HttpResponse(json.dumps(infoAndReviews))
