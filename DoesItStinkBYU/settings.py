3f07b2169ea9f35c51b3b4978d3a52d4d3205f52
Trevor Schmidt <trevorjschmidt97@gmail.com>
2020-12-08 21:46:36 -0700

    fixed

--- DoesItStinkBYU/settings.py
+++ DoesItStinkBYU/settings.py
@@ -101,8 +101,8 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env) 
+# db_from_env = dj_database_url.config(conn_max_age=600)
+# DATABASES['default'].update(db_from_env) 
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
